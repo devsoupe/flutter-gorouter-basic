4f23639b07ccbe6a7bbe7c87147b7abc2240c0b1
devsoupe <luvyum4e@gmail.com>
2023-09-18 22:24:39 +0900

    Go Named 함수

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_gorouter_basic/screens/1_basic_screen.dart';
+import 'package:flutter_gorouter_basic/screens/2_named_screen.dart';
 import 'package:flutter_gorouter_basic/screens/root_screen.dart';
 import 'package:go_router/go_router.dart';
 
@@ -6,6 +7,7 @@ import 'package:go_router/go_router.dart';
 // https://blog.codefactory.ai/flutter -> /flutter
 // / -> home
 // /basic -> basic screen
+// /named -> named screen
 final router = GoRouter(
   routes: [
     GoRoute(
@@ -18,6 +20,13 @@ final router = GoRouter(
           path: 'basic',
           builder: (context, state) {
             return const BasicScreen();
+          },
+        ),
+        GoRoute(
+          path: 'named',
+          name: 'named_screen',
+          builder: (context, state) {
+            return const NamedScreen();
           },
         ),
       ],
